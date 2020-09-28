f4d1bb7d7bce9cdda5cf57c7bb13023badd59e91
Albert Chen <adc054@eng.ucsd.edu>
2020-09-28 15:38:24 -0700

    add default search

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import SearchItem from './search_item';
+import { Link } from "react-router-dom";
 
 class SearchProducts extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            search: '',
-            showDropdown: false,
-            defaultSearch: [
-                "Men's Shoes",
-                "Women's Shoes",
-                "Basketball Shoes",
-                "Running Shoes",
-                "Casual Shoes"
-            ]
-        }
+          search: "Search",
+          showDropdown: false,
+          defaultSearch: [
+            "Men's Basketball Shoes",
+            "Men's Running Shoes",
+            "Men's Casual Shoes",
+            "Women's Basketball Shoes",
+            "Women's Running Shoes",
+            "Women's Casual Shoes",
+          ],
+          defaultText: "Search",
+        };
 
         this.openDropdown = this.openDropdown.bind(this);
         this.closeDropdown = this.closeDropdown.bind(this);
+        this.clearInput = this.clearInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount(){
         this.props.getProducts();
     }
-    // componentDidUpdate() {
-    //     if (this.props.products.length === 1) {
-    //         this.props.getProducts();
-    //         this.state.search = '';
-    //     }
-    // }
+    componentDidUpdate() {
+        if (this.props.products.length === 1) {
+            this.props.getProducts();
+            // this.state.search = '';
+        }
+    }
 
     handleInput(){
         return(e) => {
@@ -36,11 +41,15 @@ class SearchProducts extends React.Component{
         }
     }
 
+    clearInput() {
+        this.setState({search: 'Search'})
+    }
+
     checkFiltered() {
         const { products } = this.props;
-
+        
         let filteredItems = products.filter(product => {
-            if (this.state.search.length == 0) {
+            if (this.state.search.length === 0) {
                 return false;
             }
            if (product.product_name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1) {
@@ -49,17 +58,28 @@ class SearchProducts extends React.Component{
            
         })
         filteredItems = filteredItems.slice(0, 5);
+
         let filtered;
-        if (products.length === 0) {
-            return null;
+        if (this.state.search.length === 0) {
+            filtered = this.state.defaultSearch.map((item, idx) => {
+                return (
+                    <div key={idx}>
+                        {item}
+                    </div>
+                )
+            })
+            // return null;
         } else {
             filtered = (
                 filteredItems.map(product => {
                     return (
-                        <div key={product.id}>
-                            <SearchItem product={product}/>
-                        </div>
-                    )
+                      <div key={product.id}>
+                        {/* <SearchItem product={product} state={this.state}/> */}
+                        <Link to={`/products/${product.id}`} onClick={this.clearInput}>
+                          {product.product_name}
+                        </Link>
+                      </div>
+                    );
                 })
             )
         }
@@ -69,20 +89,33 @@ class SearchProducts extends React.Component{
     openDropdown() {
         this.setState({showDropdown: true}, () => {
             document.addEventListener('click', this.closeDropdown);
+            if (this.state.defaultText === this.state.search) {
+                this.setState({search: ''});
+            }
         })
     }
 
     closeDropdown() {
         this.setState({showDropdown:false}, () => {
             document.removeEventListener('click', this.closeDropdown);
+            if (this.state.search === '') {
+                this.setState({ search: 'Search'});
+            }
         })
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
     render() {
+
         return (
             <div>
-                <form action="">
-                    <input type="text" onChange={this.handleInput()} onClick={this.openDropdown} placeholder='Search'></input>
+                <form id='test' action="">
+                    <i className="fas fa-search"></i>
+                    <input type="text" onChange={this.handleInput()} onClick={this.openDropdown} value={this.state.search}></input>
                     <ul>
                         {this.state.showDropdown ? this.checkFiltered() : null}
                     </ul>
--- frontend/components/search/search_item.jsx
+++ frontend/components/search/search_item.jsx
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 
 const SearchItem = (props) => {
     let {product} = props;
-
     return (
       <div>
         <Link to={`/products/${product.id}`}>{product.product_name}</Link>
