0f8eabdc24a7471ebe53922882a9cef043cc4f87
Albert Chen <adc054@eng.ucsd.edu>
2020-10-01 16:13:56 -0700

    style your reviews

--- frontend/components/reviews/reviews_index.jsx
+++ frontend/components/reviews/reviews_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-
+import {Link} from 'react-router-dom';
+import StarRatings from 'react-star-ratings';
 
 class ReviewIndex extends React.Component{
     constructor(props) {
@@ -19,18 +19,32 @@ class ReviewIndex extends React.Component{
         if (Object.values(this.props.products).length === 0) return null;
 
         return(
-            <div style={{top: '100px', position:'relative'}}>
-                <h1>Your Reviews</h1>
+            <div className='your-reviews-container'>
+                <h1 className='your-reviews-header'>Your Reviews</h1>
                 {reviews.map( review => {
                     return (
-                        <div key={review.id}>
-                            <img src={this.props.products[review.product_id].photoUrls[0]}/>
-                            {this.props.products[review.product_id].product_name}
-                            <div>{review.title}</div>
-                            <div>{review.body}</div>
-                            <div>{review.rating}</div>
-                            <Link to={`/reviews/${review.id}`}>Edit</Link>
-                            <button onClick={() => {this.props.deleteReview(review.id); window.location.reload()}}>Delete</button>
+                        <div key={review.id} className='your-reviews-element'>
+
+                            <img className='your-reviews-image' src={this.props.products[review.product_id].photoUrls[0]}/>
+                            <div className='your-reviews-info'>
+                                <div className='your-reviews-product'>{this.props.products[review.product_id].product_name}</div>
+                                <div className='your-reviews-title'>{review.title}</div>
+                                <div className='your-reviews-body'>{review.body}</div>
+                                <div className='your-reviews-rating'>
+                                    Rating: &nbsp;
+                                    <StarRatings 
+                                        rating={parseFloat(review.rating)}
+                                        ignoreInlineStyles={false}
+                                        starDimension='15px'
+                                        starRatedColor='gold'
+                                        numberOfStars={5}
+                                        name='rating'
+                                        starSpacing='1px'
+                                    />
+                                </div>
+                                <Link to={`/reviews/${review.id}`}>Edit</Link>
+                                <button onClick={() => {this.props.deleteReview(review.id); window.location.reload()}}>Delete</button>
+                            </div>
 
                         </div>
                     )
