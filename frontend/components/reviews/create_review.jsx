d82934b232b45a7a9f4d24445449e5fe36b06eb3
Albert Chen <adc054@eng.ucsd.edu>
2020-10-04 11:25:10 -0700

    style create review

--- frontend/components/reviews/create_review.jsx
+++ frontend/components/reviews/create_review.jsx
@@ -48,42 +48,45 @@ class CreateReview extends React.Component {
         return (
             <div className='popup'>
                 <div className='popup_inner'>
-                    <form onSubmit={this.handleSubmit}> 
-                        <h1>Write a Review!</h1>
+                    <form onSubmit={this.handleSubmit} className='create-review-form'> 
+                        <h1 className='create-review-header'>Write a Review!</h1>
                         <label>
-                            <input type="text" value={this.state.title} onChange={this.update('title')} onClick={this.clearInput('title')}/>
+                            <input className='create-review-title' type="text" value={this.state.title} onChange={this.update('title')} onClick={this.clearInput('title')}/>
                         </label>
 
                         <label>
-                            <textarea value={this.state.body} onChange={this.update('body')} onClick={this.clearInput('body')}/>
+                            <textarea className='create-review-body' value={this.state.body} onChange={this.update('body')} onClick={this.clearInput('body')}/>
                         </label>
-                        <div>Rate your experience!</div>
-                        <span className='star-cb-group'>
-                            <input id='rating-5' type="radio" value='5' name='rating' checked={(this.state.rating === 5)} onChange={this.updateRating('rating')}/>
-                            <label htmlFor='rating-5'>5
-                            </label>
-                            <input id='rating-4' type="radio" value='4' name='rating' checked={(this.state.rating === 4)}onChange={this.updateRating('rating')}/>
-                            <label htmlFor='rating-4'>4
-                            </label>
-                                <input id='rating-3' type="radio" value='3' name='rating' checked={(this.state.rating === 3)} onChange={this.updateRating('rating')}/>
-                            <label htmlFor='rating-3'>3
-                            </label>
-                                <input id='rating-2' type="radio" value='2' name='rating' checked={(this.state.rating === 2)} onChange={this.updateRating('rating')}/>
-                            <label htmlFor='rating-2'>2
-                            </label>
-                                <input id='rating-1' type="radio" value='1' name='rating' checked={(this.state.rating === 1)} onChange={this.updateRating('rating')}/>
-                            <label htmlFor='rating-1'>1
-                            </label>
-                                <input id='rating-0' type="radio" value='0' name='rating' checked={(this.state.rating === 0)} onChange={this.updateRating('rating')} className='star-cb-clear'/>
-                            <label htmlFor='rating=-'>0
-                            </label>
-                        </span>
+                        <div className='create-review-rate'>Rate your experience!</div>
+                        <div className='create-review-stars'>
+                            <span className='star-cb-group'>
+                                
+                                <input id='rating-5' type="radio" value='5' name='rating' checked={(this.state.rating === 5)} onChange={this.updateRating('rating')}/>
+                                <label htmlFor='rating-5'>5
+                                </label>
+                                <input id='rating-4' type="radio" value='4' name='rating' checked={(this.state.rating === 4)}onChange={this.updateRating('rating')}/>
+                                <label htmlFor='rating-4'>4
+                                </label>
+                                    <input id='rating-3' type="radio" value='3' name='rating' checked={(this.state.rating === 3)} onChange={this.updateRating('rating')}/>
+                                <label htmlFor='rating-3'>3
+                                </label>
+                                    <input id='rating-2' type="radio" value='2' name='rating' checked={(this.state.rating === 2)} onChange={this.updateRating('rating')}/>
+                                <label htmlFor='rating-2'>2
+                                </label>
+                                    <input id='rating-1' type="radio" value='1' name='rating' checked={(this.state.rating === 1)} onChange={this.updateRating('rating')}/>
+                                <label htmlFor='rating-1'>1
+                                </label>
+                                    <input id='rating-0' type="radio" value='0' name='rating' checked={(this.state.rating === 0)} onChange={this.updateRating('rating')} className='star-cb-clear'/>
+                                <label htmlFor='rating=-'>0
+                                </label>
+                            </span>
+                        </div>
 
                         <div>
-                            <div className='checkout'>
-                                <button type='submit' onClick={() => {this.props.closePopup; window.location.reload(false);}}>Submit Review</button>
+                            <div className='review-button-div'>
+                                <button className='create-review-button' type='submit' onClick={() => {this.props.closePopup; window.location.reload(false);}}>Submit Review</button>
                             </div>
-                            <Link to={`/products/${this.state.product_id}`}><div className='login-close' type='submit' onClick={() => {this.props.closePopup; window.location.reload(false);}}></div></Link>
+                            <Link to={`/products/${this.state.product_id}`}><div className='review-close' type='submit' onClick={() => {this.props.closePopup; window.location.reload(false);}}></div></Link>
                         </div>
                     </form>
                 </div>
